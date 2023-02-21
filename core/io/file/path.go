de0d0d77bdf311687803daf84c3b77e94e36a153
Ryan SU <1137661202@qq.com>
2023-02-21 19:29:31 +0800

    feat: path functions

--- core/io/file/path.go
+++ core/io/file/path.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The Ryan SU Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package file
 
 import (
@@ -95,4 +109,114 @@ func RemoveDir(path string) error {
 	}
 
 	return nil
+}
+
+// ContainFile returns true if there is the file under the directory path.
+func ContainFile(dirPath, fileName string) (bool, error) {
+	fileNameData, err := GetFilesPathFromDir(dirPath, true)
+	if err != nil {
+		return false, err
+	}
+
+	for _, v := range fileNameData {
+		if v == fileName {
+			return true, err
+		}
+	}
+
+	return false, err
+}
+
+// FileExist returns true if the specified file exists.
+func FileExist(file string) bool {
+	_, err := os.Stat(file)
+	return err == nil
+}
+
+// CreateIfNotExist creates a file if it does not exist.
+func CreateIfNotExist(filePath string) (*os.File, error) {
+	if FileExist(filePath) {
+		return nil, fmt.Errorf("%s already exist", filePath)
+	}
+
+	return os.Create(filePath)
+}
+
+// RemoveIfExist deletes the specified file if exists.
+func RemoveIfExist(filename string) error {
+	if !FileExist(filename) {
+		return nil
+	}
+
+	return os.Remove(filename)
+}
+
+// GetSubDir returns subdirectory path slice from a directory.
+// If onlyName is true, it will only return name slice.
+func GetSubDir(path string, onlyName bool) (result []string, err error) {
+	if len(path) == 0 {
+		return nil, errors.New("the path can not be empty")
+	}
+	if _, err := os.Stat(path); err != nil {
+		return nil, err
+	}
+	_ = filepath.WalkDir(path, func(pathData string, d fs.DirEntry, err error) error {
+		if d.IsDir() && filepath.Dir(pathData) == path {
+			if onlyName {
+				result = append(result, filepath.Base(pathData))
+			} else {
+				result = append(result, pathData)
+			}
+			return err
+		}
+		return err
+	})
+	return result, nil
+}
+
+// ReadFileString returns string from a file
+func ReadFileString(path string) (string, error) {
+	if FileExist(path) {
+		fileData, _ := os.ReadFile(path)
+		return string(fileData), nil
+	} else {
+		return "", errors.New("file does not exist")
+	}
+}
+
+// WriteFileString writes string to a file, if file does not exist, it will
+// be created.
+func WriteFileString(path, data string, perm int) (err error) {
+	var fileData *os.File
+	if FileExist(path) {
+		fileData, err = os.OpenFile(path, os.O_RDWR, fs.FileMode(perm))
+		if err != nil {
+			return err
+		}
+	} else {
+		fileData, _ = CreateIfNotExist(path)
+	}
+	defer fileData.Close()
+
+	_, err = fileData.Write([]byte(data))
+	return err
+}
+
+// AppendFileString append the string to the target file
+func AppendFileString(path, data string, perm int) error {
+	if FileExist(path) {
+		fileData, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, fs.FileMode(perm))
+		if err != nil {
+			return err
+		}
+		defer fileData.Close()
+
+		_, err = fileData.Write([]byte(data))
+		if err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return errors.New("file does not exist")
+	}
 }
--- core/io/file/path_test.go
+++ core/io/file/path_test.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The Ryan SU Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package file
 
 import (
@@ -25,7 +39,7 @@ func createTempFiles() error {
 		if err != nil {
 			return err
 		}
-		_, err = file.Write([]byte("hello word"))
+		_, err = file.Write([]byte("hello world"))
 		if err != nil {
 			return err
 		}
@@ -82,6 +96,10 @@ func TestGetFilesPathFromDir(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, 4, len(dir))
 
+	dir, err = GetFilesPathFromDir(tmpDirPath, true)
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(dir))
+
 	dir, err = GetFilesPathFromDir("", false)
 	assert.NotNil(t, err)
 
@@ -101,6 +119,130 @@ func TestRemoveDir(t *testing.T) {
 	err = RemoveDir("")
 	assert.NotNil(t, err)
 
-	err = RemoveDir("////")
+	err = RemoveDir("/tmp/test_data")
+	assert.NotNil(t, err)
+}
+
+func TestContainFile(t *testing.T) {
+	err := createTempFiles()
+	assert.Nil(t, err)
+
+	tmpDirPath := filepath.Join(os.TempDir(), "knife_test")
+
+	fileExist, err := ContainFile(tmpDirPath, "1.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, fileExist, true)
+
+	fileExist, err = ContainFile(tmpDirPath, "10.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, fileExist, false)
+
+	fileExist, err = ContainFile("", "10.txt")
+	assert.NotNil(t, err)
+}
+
+func TestFileExists(t *testing.T) {
+	err := createTempFiles()
+	assert.Nil(t, err)
+
+	tmpDirPath := filepath.Join(os.TempDir(), "knife_test")
+
+	result := FileExist(filepath.Join(tmpDirPath, "1.txt"))
+	assert.Equal(t, true, result)
+
+	result = FileExist(filepath.Join(tmpDirPath, "10.txt"))
+	assert.Equal(t, false, result)
+}
+
+func TestCreateIfNotExist(t *testing.T) {
+	tmpFilePath := filepath.Join(os.TempDir(), "knife_test_create", "5.txt")
+	err := MkdirIfNotExist(filepath.Join(os.TempDir(), "knife_test_create"), SuperPerm)
+	assert.Nil(t, err)
+
+	file, err := CreateIfNotExist(tmpFilePath)
+	assert.Nil(t, err)
+	file.Close()
+
+	file2, err := CreateIfNotExist(tmpFilePath)
+	assert.NotNil(t, err)
+	file2.Close()
+
+	err = RemoveIfExist(tmpFilePath)
+	assert.Nil(t, err)
+
+	err = RemoveIfExist(tmpFilePath)
+	assert.Nil(t, err)
+}
+
+func TestGetSubDir(t *testing.T) {
+	tmpDirPath := filepath.Join(os.TempDir(), "knife_test_mkdir")
+	err := MkdirIfNotExist(tmpDirPath+"/1", SuperPerm)
+	assert.Nil(t, err)
+
+	err = MkdirIfNotExist(tmpDirPath+"/1/2", SuperPerm)
+	assert.Nil(t, err)
+
+	dir, err := GetSubDir(tmpDirPath, false)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(dir))
+
+	dir, err = GetSubDir(tmpDirPath, true)
+	assert.Nil(t, err)
+	assert.Equal(t, "1", dir[0])
+
+	dir, err = GetSubDir("", false)
+	assert.NotNil(t, err)
+
+	dir, err = GetSubDir("/knife", false)
+	assert.NotNil(t, err)
+
+	err = RemoveDir(tmpDirPath)
+	assert.Nil(t, err)
+}
+
+func TestReadFileString(t *testing.T) {
+	err := createTempFiles()
+	assert.Nil(t, err)
+
+	tmpDirPath := filepath.Join(os.TempDir(), "knife_test")
+
+	fileString, err := ReadFileString(filepath.Join(tmpDirPath, "1.txt"))
+	assert.Nil(t, err)
+	assert.Equal(t, "hello world", fileString)
+
+	fileString, err = ReadFileString("")
+	assert.NotNil(t, err)
+}
+
+func TestWriteFileString(t *testing.T) {
+	err := createTempFiles()
+	assert.Nil(t, err)
+
+	tmpDirPath := filepath.Join(os.TempDir(), "knife_test")
+
+	err = RemoveIfExist(filepath.Join(tmpDirPath, "0.txt"))
+	assert.Nil(t, err)
+
+	err = WriteFileString(filepath.Join(tmpDirPath, "0.txt"), "Simple Admin", SuperPerm)
+	assert.Nil(t, err)
+
+	err = WriteFileString(filepath.Join(tmpDirPath, "0.txt"), "Simple Admin", SuperPerm)
+	assert.Nil(t, err)
+
+	fileString, err := ReadFileString(filepath.Join(tmpDirPath, "0.txt"))
+	assert.Nil(t, err)
+	assert.Equal(t, "Simple Admin", fileString)
+}
+
+func TestAppendFileString(t *testing.T) {
+	err := createTempFiles()
+	assert.Nil(t, err)
+
+	tmpDirPath := filepath.Join(os.TempDir(), "knife_test")
+
+	err = AppendFileString(filepath.Join(tmpDirPath, "2.txt"), "Hi!", SuperPerm)
+	assert.Nil(t, err)
+
+	err = AppendFileString(filepath.Join(tmpDirPath, "20.txt"), "Hi!", SuperPerm)
 	assert.NotNil(t, err)
 }
